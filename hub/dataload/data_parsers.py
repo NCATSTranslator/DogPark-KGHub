f3e259826e770cee32b164687da2f20353569c5d
shuchenliu <shuchenliu2014@gmail.com>
2025-09-09 20:50:47 -0700

    add gz parsing

--- hub/dataload/data_parsers.py
+++ hub/dataload/data_parsers.py
@@ -1,13 +1,36 @@
+import gzip
 import pathlib
+from contextlib import contextmanager
+from functools import partial
+
 import jsonlines
-from typing import Union
+from typing import Union, Literal
 
 NODE_BUFFER_SIZE = 4096
 EDGE_BUFFER_SIZE = 2048
 
+@contextmanager
+def gz_open(path: Union[str, pathlib.Path]):
+    """Open a gzipped JSONL file with jsonlines.Reader."""
+    with gzip.open(path, "rt") as f:
+        reader = jsonlines.Reader(f)
+        try:
+            yield reader
+        finally:
+            reader.close()
+
 def read_jsonl(input_file: Union[str, pathlib.Path]):
     """ Common reader to load data from jsonl files """
-    with jsonlines.open(input_file) as source:
+
+    gzip_file = input_file.with_name(input_file.name + ".gz")
+
+    file_loader = jsonlines.open
+
+    if pathlib.Path(gzip_file).exists():
+        input_file = gzip_file
+        file_loader = gz_open
+
+    with file_loader(input_file) as source:
         buffer = []
         index = 0
         for doc in source:
@@ -25,18 +48,21 @@ def read_jsonl(input_file: Union[str, pathlib.Path]):
             yield from buffer
 
 
+def loader(data_folder: Union[str, pathlib.Path], entity: Literal['edges', 'nodes']):
+    """ Meta loader to stream edge data from given JSONL file """
+    data_folder = pathlib.Path(data_folder).resolve().absolute()
+    edge_file = data_folder.joinpath(f"{entity}.jsonl")
+    yield from read_jsonl(edge_file)
+
+
 def load_edges(data_folder: Union[str, pathlib.Path]):
     """ Stream edge data from given JSONL file """
-    data_folder = pathlib.Path(data_folder).resolve().absolute()
-    edge_file = data_folder.joinpath("edges.jsonl")
-    yield from read_jsonl(edge_file)
+    yield from loader(data_folder, "edges")
 
 
 def load_nodes(data_folder: Union[str, pathlib.Path]):
     """ Stream node data from given JSONL file """
-    data_folder = pathlib.Path(data_folder).resolve().absolute()
-    edge_file = data_folder.joinpath("nodes.jsonl")
-    yield from read_jsonl(edge_file)
+    yield from loader(data_folder, "nodes")
 
 
 def load_merged_edges(data_folder: Union[str, pathlib.Path]):
